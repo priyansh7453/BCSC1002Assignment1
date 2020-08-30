d7220d1b4e8243e2a3e19c3eb8f1d4d46186fc0a
Priyansh <priyansh.kushwaha_cs19@gla.ac.in>
2020-08-30 19:19:00 +0530

    student input condition

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -27,5 +27,45 @@ public class FrontDesk {
         Student studentObject = new Student();
         Book bookObject = new Book();
 
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?\n");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter you choice (1..4): ");
+            studentInput = scannerObject.nextInt();
+            switch (studentInput) {
+                case ISSUE_A_NEW_BOOK_FOR_ME:
+                    scannerObject.nextLine();
+                    System.out.println("Enter the name of the books that you want to issue: ");
+                    String[] bookNames = new String[5];
+                    for (int i = 0; i < 5; i++) {
+                        bookNames[i] = scannerObject.next();
+                    }
+                    for (String bookName : bookNames) {
+                        System.out.println(bookName);
+                    }
+
+                    break;
+                case RETURN_A_PREVIOUSLY_ISSUED_BOOK_FOR_ME:
+                    System.out.println("Enter the name of the name of book that you wan to return: ");
+                    scannerObject.nextLine();
+                    //videoName = scanner.nextLine();
+                    //myVideoStore.doCheckout(videoName);
+                    break;
+                case SHOW_ME_ALL_MY_ISSUES_BOOKS:
+                    System.out.println("Your all issues book names are: ");
+                    scannerObject.nextLine();
+                    //videoName = scanner.nextLine();
+                    //myVideoStore.doReturn(videoName);
+                    break;
+
+                default :
+            }
+        } while (studentInput != EXIT);
+        scannerObject.close();
+    }
 
     }
