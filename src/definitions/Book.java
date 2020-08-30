3a92d386d6690a07becd44e45cd8b530446f2ed5
Priyansh <priyansh.kushwaha_cs19@gla.ac.in>
2020-08-30 12:29:59 +0530

    make a private fields.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,24 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
- *  File Name : Book.java
- * */
-package definitions;
+  /*  Created by IntelliJ IDEA.
+   *  User: Priyansh kushwaha
+   *  Date: 30/08/20
+   *  Time: 3:50 PM
+   *  File Name : Book.java
+   * */
+  package definitions;
+
+   import java.util.Objects;
 
 public class Book {
+
+   //  component of a definition class in java.
+    // 1. private fields.
+    // 2. getter methods for private fields.
+    // 3. setter methods for private fields.
+    // 4. Constructor for initializing the fields of an objects.
+    // 5. the toString() methods.
+    // 6. th equals() and the hashCode() methods.
+
+    private String nameOfBook;
+    private String authorOfBook;
+    private String thirteenDIgitsISBNNumberOfBook;
 }
