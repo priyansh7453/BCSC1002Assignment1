7f9ddfde061f74712b2a58f1f76cf29c71c7d65c
Priyansh <priyansh.kushwaha_cs19@gla.ac.in>
2020-08-30 16:31:18 +0530

    create Library.java and library method

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,34 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: Priyansh Kushwaha
+ *  Date: 30/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
+    private Book[] stock;
+
+    public Library() {
+        this.stock = new Book[10];
+        for (int index = 0; index < stock.length; index++) {
+            stock[index] = new Book("AvailableBook " + (index + 1));
+        }
+    }
+
+    public Library(Book[] stock) {
+        this.stock = stock;
+    }
+
+    public Book[] getStock() {
+        return this.stock;
+    }
+
+    public void setStock(Book[] stock) {
+        this.stock = stock;
+    }
+
+
 }
