560d8cd38d64e60a99ff9aa9c2572d93ffb2c62d
Priyansh <priyansh.kushwaha_cs19@gla.ac.in>
2020-08-30 18:43:58 +0530

    return name of Student with first,middle,last

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -42,5 +42,68 @@ public class Student {
                         name[index] = new Book("Book " + (index + 1));
                 }
         }
+        public Student(String studentFirstName, String studentMiddleName, String studentLastName) {
+                this.studentFirstName = studentFirstName;
+                this.studentMiddleName = studentMiddleName;
+                this.studentLastName = studentLastName;
+        }
+
+        //add getter and setter methods to read and write the value of private fields respectively
+
+        /**
+         * This method returns the first name of the student.
+         *
+         * @return The first name of the student.
+         */
+
+        public String getStudentFirstName() {
+                return this.studentFirstName;
+        }
+
+        /**
+         * This method sets the first name of student.
+         *
+         * @param studentFirstName The first name of the student that you want to set.
+         */
+
+        public void setStudentFirstName(String studentFirstName) {
+                this.studentFirstName = studentFirstName;
+        }
+
+        /**
+         * This method returns the middle name of the student.
+         *
+         * @return The middle name of the student.
+         */
+
+        public String getStudentMiddleName() {
+                return this.studentMiddleName;
+        }
+
+        /**
+         * This method sets the middle name of student.
+         *
+         * @param studentMiddleName The middle name of the student that you want to set.
+         */
+
+        public void setStudentMiddleName(String studentMiddleName) {
+                this.studentMiddleName = studentMiddleName;
+        }
+
+        /**
+         * This method returns the last name of the student.
+         *
+         * @return The last name of the student.
+         */
+
+        public String getStudentLastName() {
+                return this.studentLastName;
+        }
+        /**
+         * This method sets the last name of student.
+         *
+         * @param studentLastName The last name of the student that you want to set.
+         */
+
 
 }
